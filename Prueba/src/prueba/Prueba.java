c781133f1a4cfeb12cb0a6268b1b82a610d2cb18
granadino00 <carlosxp666@gmail.com>
2022-03-24 20:23:21 +0100

    v1

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("");
+        System.out.println("hola");
     }
     
 }
