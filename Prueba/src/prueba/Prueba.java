ea915feab19cacf8ccc2533b1dbb49386bd68066
granadino00 <carlosxp666@gmail.com>
2022-03-28 19:53:42 +0200

    Commit de mi segundo dia de trabajo

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("hola");
+        System.out.println("Segundo dia de trabajo");
     }
     
 }
